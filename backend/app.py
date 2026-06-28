6c0cf07c854f89a034e5d2d6841d209a63287322
Vishman <vishman1923@gmail.com>
2026-06-28 19:01:53 +0530

    Fix IST timezone

--- backend/app.py
+++ backend/app.py
@@ -117,8 +117,7 @@ def submit_grievance():
         VALUES (?, ?, ?, ?, ?, ?, ?)''',
         (data['student_name'], data['roll_number'], data['department'],
          data['grievance_type'], data['description'], priority,
-         datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-    conn.commit()
+         datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=5, minutes=30))).strftime("%Y-%m-%d %H:%M:%S")
     conn.close()
     return jsonify({'message': 'Grievance submitted!', 'priority': priority})
 
